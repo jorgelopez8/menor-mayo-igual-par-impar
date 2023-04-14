28eb8abe61e57aa03b282109e1bf1772a53c4d4c
jorgelopez8 <123724150+jorgelopez8@users.noreply.github.com>
2023-04-14 09:44:59 +0200

    script

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let num1 = parseFloat(prompt("Introduzca aquí el primer número:"));
 let num2 = parseFloat(prompt("Ahora introduzca el siguiente número"));
 
 if (num1>num2) {
-    alert (num1 + "Es mayor")
+    alert("El " + num1 + "es mayor que el " + num2)
 }
-else if (num1<num2) {
-    alert (num2 + "Es mayor")
+else if (num1<num2){
+    alert("El " + num2 + "es mayor que el " + num1)
 }
-else if (num1==num2) {
-    alert (num1 + "&&"  + num2 + "Son iguales")
+else if(num1==num2){
+    alert("El "+ num1 + "y el " + num2 + "son iguales ")
 }
-if (num2 % 2== 0) {
-    alert(num2 + "Es par")
+if (num2 % 2 == 0){
+    alert("El " + num2 + "número es par")
 }
-else if (num2 % 2== 1) {
-    alert(num2 + "Es impar")
+else{
+    alert("El " + num2 + "número es impar")
 }
